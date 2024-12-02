6db954e5a1f15c79b20f98dabc9dad9b42d37fad
Rynnnnnnnn <163542461+Rynnnnnnnn@users.noreply.github.com>
2024-12-02 16:04:04 +0800

    data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -18,11 +18,18 @@ CREATE TABLE applicants (
     CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     dob DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    operation VARCHAR(50) NOT NULL,
+    details TEXT,
+    performed_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
