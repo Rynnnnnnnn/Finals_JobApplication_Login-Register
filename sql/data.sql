d59a006f93a35094d4e53e8537393b69df1dbb89
Rynnnnnnnn <163542461+Rynnnnnnnn@users.noreply.github.com>
2024-12-02 16:09:40 +0800

    data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE job_application_system;
-
-USE job_application_system;
-
 CREATE TABLE applicants (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
@@ -10,12 +6,12 @@ CREATE TABLE applicants (
     phone_number VARCHAR(15),
     specialization VARCHAR(100),
     experience_years INT,
-    application_date DATE DEFAULT CURRENT_DATE
+    application_date DATE DEFAULT CURRENT_DATE,
     last_added_by VARCHAR(255) DEFAULT NULL,
-    last_updated_by VARCHAR(255) DEFAULT NULL;
+    last_updated_by VARCHAR(255) DEFAULT NULL
 );
 
-    CREATE TABLE users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -32,4 +28,3 @@ CREATE TABLE logs (
     details TEXT,
     performed_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
